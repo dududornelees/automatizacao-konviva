66afc63da4955c8006fd9d1b39da692bfeaf644e
dududornelees <eduardo.dsdorneles@gmail.com>
2022-01-17 11:38:02 -0300

    Fix: Novos arquivos

--- Konviva.py
+++ Konviva.py
@@ -5,14 +5,14 @@ import os
 # Array com os arquivos à serem baixados
 arquivos = [
     {"nome": "Maven - 3.6.3", "endereco": "maven.bin.zip", "url": "https://dlcdn.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip"},
-    {"nome": "Tomcat - 10.0.14", "endereco": "tomcat.zip", "url": "https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.14/bin/apache-tomcat-10.0.14-windows-x64.zip"},
+    {"nome": "Tomcat - 9.0.56", "endereco": "tomcat.zip", "url": "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.56/bin/apache-tomcat-9.0.56-windows-x64.zip"},
     {"nome": "Maria DB - 10.6", "endereco": "maria-db.msi", "url": "https://espejito.fder.edu.uy/mariadb//mariadb-10.6.5/winx64-packages/mariadb-10.6.5-winx64.msi"},
     {"nome": "Mongo DB - 5.0.5", "endereco": "mongo-db.msi", "url": "https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-5.0.5-signed.msi"},
     {"nome": "Heidi SQL - 11.3", "endereco": "heidi-sql.exe", "url": "https://www.heidisql.com/installers/HeidiSQL_11.3.0.6295_Setup.exe"},
     {"nome": "OpenVPN - 2.5.5", "endereco": "open-vpn.msi", "url": "https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.5-I602-amd64.msi"},
     {"nome": "NodeJS - 14.7.3", "endereco": "node-js.msi", "url": "https://nodejs.org/download/release/v14.17.3/node-v14.17.3-x64.msi"},
     {"nome": "Java - 8", "endereco": "java.exe", "url": "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245448_4d5417147a92418ea8b615e228bb6935"},
-    {"nome": "JDK - 1.8", "endereco": "jdk.exe", "url": "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_202-b08/1961070e4c9b4e26a04e7f5a083f551e/windows-i586/jdk-8u202-windows-x64.exe"}
+    {"nome": "JDK - 1.8 301", "endereco": "jdk.exe", "url": "https://download.oracle.com/otn/java/jdk/8u301-b09/d3c52aa6bfa54d3ca74e617f18309292/jdk-8u301-windows-x64.exe?AuthParam=1642430249_35b16b502ea9dd1873d117017ab3c3b0"}
 ]
 
 # Função para baixar e abrir os arquivos
